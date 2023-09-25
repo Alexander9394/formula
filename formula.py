b002b3bdbccab4c703abf40bc3b74c702b3cf385
Alexander Vingovatov <vingovatov.2022@stud.nstu.ru>
2023-09-25 16:40:49 +0700

    formula

---
+++ formula.py
@@ -0,0 +1,9 @@
+import math
+
+a = float(input('Введите ваше число:'))
+
+z1 = ((math.cos(a))+(math.sin(a))+(math.cos(3*a))+(math.sin(3*a)))
+z2 = ((math.sqrt(2/2))+(math.cos(a))*(math.sin)(math.pi/4+2*a))
+
+print (z1)
+print (z2)
