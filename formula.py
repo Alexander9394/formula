a3edf87222c1a604f2ced13f391e10c7b090ba95
Alexander Vingovatov <vingovatov.2022@stud.nstu.ru>
2023-09-28 18:55:50 +0700

    'formula'

--- formula.py
+++ formula.py
@@ -1,9 +1,9 @@
 import math
 
-a = float(input('Введите ваше число:'))
+a = float(input('Ведите a:'))
 
-z1 = ((math.cos(a))+(math.sin(a))+(math.cos(3*a))+(math.sin(3*a)))
-z2 = ((math.sqrt(2/2))+(math.cos(a))*(math.sin)(math.pi/4+2*a))
+z1 = math.cos(a)+math.sin(a)+math.cos(3*a)+math.sin(3*a)
+z2 = 2*math.sqrt(2)*math.cos(a)*math.sin(math.pi/4+2*a)
 
-print (z1)
-print (z2)
+print(f'z1 = {z1}')
+print(f'z1 = {z1}')
